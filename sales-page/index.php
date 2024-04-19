3bbebfdb08c94c46941d3dde066588913d22b071
sulthony.akbar <sulthonyakbar18@gmail.com>
2024-04-19 16:20:08 +0700

    [FIX] First commit

---
+++ sales-page/index.php
@@ -0,0 +1,155 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" integrity="sha512-1ycn6IcaQQ40/MKBW2W4Rhis/DbILU74C1vSrLJxCq57o941Ym01SwNsOMqvEBFlcgUa6xLiPY/NS5R+E6ztJQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="shortcut icon" href="" type="image/x-icon">
+    <script src="https://cdn.tailwindcss.com"></script>
+    <title>Sales Page - Spookids</title>
+</head>
+
+<body>
+    <!-- Headline -->
+    <div class="bg-1 container-lg">
+        <div class="flex flex-col items-center justify-center">
+            <div class="flex flex-col items-center justify-center px-5 md:px-24">
+                <h3 class="montserrat-medium text-[#F3E9D3] text-center font-1-2rem mt-16 md:w-[80%]">
+                    ENGAGE AND ENTERTAIN: KEEP YOUR KIDS GLUED TO THE SCREEN WITH OUR CHILLING SHORT HORROR STORIES
+                </h3>
+
+                <h1 class="halloween text-[#F3E9D3] text-center leading-none font-3-5rem mt-16 md:w-[80%]">
+                    <span class="text-[#FC9850]">Spookids</span> : The Ultimate Collection of Short Horror Videos for Kids
+                </h1>
+
+                <div class="border-0 bg-[#FC9850] rounded-full w-full md:w-[80%] p-5 my-16 mx-8">
+                    <h3 class="montserrat-medium font-1rem text-center text-white">
+                        OVER 30+ SPOOKY TALES TO CHOOSE FROM: KEEP YOUR AUDIENCE ENTERTAINED AND ENGAGED
+                    </h3>
+                </div>
+            </div>
+
+            <img class="p-5 w-[100%] md:w-[50%]" src="https://placehold.co/100x100" alt="">
+
+            <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 grid-rows-3 gap-4 my-16 mx-4 md:mx-8 lg:mx-16 montserrat-bold text-start text-black">
+                <div class="border-0 bg-white drop-shadow-lg rounded-2xl px-4 md:px-5 lg:px-6 py-5 w-full flex items-center justify-self-end">
+                    <div class="order-first me-3">
+                        <img class="w-[70%]" src="img/icon-lm1.png" alt="">
+                    </div>
+                    <h3 class="font-1-2rem">
+                        Easy-to-Use Landing Page (HTML)
+                    </h3>
+                </div>
+                <div class="border-0 bg-white drop-shadow-lg rounded-2xl px-4 md:px-5 lg:px-6 py-5 w-full flex items-center justify-start">
+                    <div class="order-first me-3">
+                        <img class="w-[70%]" src="img/icon-lm3.png" alt="">
+                    </div>
+                    <h3 class="font-1-2rem">
+                        Downloadable Resources Section
+                    </h3>
+                </div>
+                <div class="border-0 bg-white drop-shadow-lg rounded-2xl px-4 md:px-5 lg:px-6 py-5 w-full flex items-center justify-self-end">
+                    <div class="order-first me-3">
+                        <img class="w-[70%]" src="img/icon-lm2.png" alt="">
+                    </div>
+                    <h3 class="font-1-2rem">
+                        Advanced Lead Capture Forms
+                    </h3>
+                </div>
+                <div class="border-0 bg-white drop-shadow-lg rounded-2xl px-4 md:px-5 lg:px-6 py-5 w-full flex items-center justify-start">
+                    <div class="order-first me-3">
+                        <img class="w-[70%]" src="img/icon-lm5.png" alt="">
+                    </div>
+                    <h3 class="font-1-2rem">
+                        Mobile-Responsive Design
+                    </h3>
+                </div>
+                <div class="border-0 bg-white drop-shadow-lg rounded-2xl px-4 md:px-5 lg:px-6 py-5 w-full flex items-center justify-self-end">
+                    <div class="order-first me-3">
+                        <img class="w-[70%]" src="img/icon-lm4.png" alt="">
+                    </div>
+                    <h3 class="font-1-2rem">
+                        High-Quality E-book & Cover Content
+                    </h3>
+                </div>
+                <div class="border-0 bg-white drop-shadow-lg rounded-2xl px-4 md:px-5 lg:px-6 py-5 w-full flex items-center justify-start">
+                    <div class="order-first me-3">
+                        <img class="w-[70%]" src="img/icon-lm6.png" alt="">
+                    </div>
+                    <h3 class="font-1-2rem">
+                        Interesting Website Content </h3>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="bg-[#F3E9D3] container-lg">
+        <div class="flex flex-col items-center justify-center text-center p-3 md:p-0">
+            <h1 class="halloween font-2-5rem mt-16">
+                Exclusive Spookids Library: Over 30+ Spooky Treats Await!
+            </h1>
+
+            <h3 class="font-1-5rem montserrat-medium my-12 w-[70%] md:w-full">
+                Limited-Time Bonus: Unlock Spooktacular Success with Re-Seller Rights!
+            </h3>
+
+            <p class="font-1rem montserrat-regular w-full md:w-[70%] mb-16">
+                Elevate your content creation to new heights with Spookids' exclusive Re-Seller Rights offer. This limited-time bonus grants you complete ownership of every video story, template, and asset within the Spookids library.
+            </p>
+
+            <a class="flex justify-center" href="">
+                <img class="mb-16 w-[90%] md:w-[40%]" src="img/cta.png" alt="">
+            </a>
+        </div>
+    </div>
+
+    <div class="bg-[#312E35] container-lg">
+        <div class="flex flex-col items-center justify-center text-center p-3 md:p-0">
+            <h1 class="halloween font-2-5rem mt-16 text-[#F3E9D3] leading-none w-full md:w-[60%]">
+                Beyond the Scroll: Data Reveals the Power of Short-Form Horror for Kids
+            </h1>
+
+
+            <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 grid-rows-2 gap-8 my-16 mx-3 md:mx-8 lg:mx-16 montserrat-bold text-start text-black">
+                <div class="bg-data p-8 md:p-12 text-center">
+                    <h3 class="halloween font-1-5rem mb-5">
+                        Short-Form Video Dominance
+                    </h3>
+                    <p class="montserrat-regular font-1rem">
+                        Data suggests shorter videos (under 10 minutes) perform best for younger audiences. Platforms like TikTok and YouTube Shorts favor short-form content.
+                    </p>
+                </div>
+                <div class="bg-data p-8 md:p-12 text-center">
+                    <h3 class="halloween font-1-5rem mb-5">
+                        Stop-Motion Animation
+                    </h3>
+                    <p class="montserrat-regular font-1rem">
+                        Studies show stop-motion animation is particularly effective for engaging younger audiences (2-7 years old) due to its unique visual style and storytelling potential.
+                    </p>
+                </div>
+                <div class="bg-data p-8 md:p-12 text-center">
+                    <h3 class="halloween font-1-5rem mb-5">
+                        Importance of Audio
+                    </h3>
+                    <p class="montserrat-regular font-1rem">
+                        Research indicates that sound design and music play a crucial role in capturing young viewers' attention and enhancing the overall storytelling experience (Source: Vidyard).
+                    </p>
+                </div>
+                <div class="bg-data p-8 md:p-12 text-center">
+                    <h3 class="halloween font-1-5rem mb-5">
+                        Emotional Connection Drives Sharing
+                    </h3>
+                    <p class="montserrat-regular font-1rem">
+                        Short-form videos for kids that spark laughter, surprise, or heartwarming feelings get shared more! (Source: Unruly)
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <?php include('privacypolicy.php') ?>
+</body>
+
+</html>
