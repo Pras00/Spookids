a6643d609ef917178ed9013d130ea3072dbaf0b3
Pras00 <prasetiawahyu22@gmail.com>
2024-04-29 15:58:29 +0700

    [FEAT] Tampilan display

---
+++ jv-page/produk.php
@@ -0,0 +1,63 @@
+<?php
+$produk = array(
+    array(
+        'title' => 'Title 1',
+        'paragraph' => 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s.',
+        'preview' => 'https://www.youtube.com/embed/y6oMutwJQCw',
+        'trailer' => 'https://www.youtube.com/embed/y6oMutwJQCw'
+    ),
+    array(
+        'title' => 'Title 2',
+        'paragraph' => 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s.',
+        'preview' => 'https://www.youtube.com/embed/y6oMutwJQCw',
+        'trailer' => 'https://www.youtube.com/embed/y6oMutwJQCw'
+    ),
+    array(
+        'title' => 'Title 3',
+        'paragraph' => 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s.',
+        'preview' => 'https://www.youtube.com/embed/y6oMutwJQCw',
+        'trailer' => 'https://www.youtube.com/embed/y6oMutwJQCw'
+    ),
+    array(
+        'title' => 'Title 4',
+        'paragraph' => 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s.',
+        'preview' => 'https://www.youtube.com/embed/y6oMutwJQCw',
+        'trailer' => 'https://www.youtube.com/embed/y6oMutwJQCw'
+    ),
+    array(
+        'title' => 'Title 5',
+        'paragraph' => 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s.',
+        'preview' => 'https://www.youtube.com/embed/y6oMutwJQCw',
+        'trailer' => 'https://www.youtube.com/embed/y6oMutwJQCw'
+    ),
+    array(
+        'title' => 'Title 6',
+        'paragraph' => 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s.',
+        'preview' => 'https://www.youtube.com/embed/y6oMutwJQCw',
+        'trailer' => 'https://www.youtube.com/embed/y6oMutwJQCw'
+    ),
+    array(
+        'title' => 'Title 7',
+        'paragraph' => 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s.',
+        'preview' => 'https://www.youtube.com/embed/y6oMutwJQCw',
+        'trailer' => 'https://www.youtube.com/embed/y6oMutwJQCw'
+    ),
+    array(
+        'title' => 'Title 8',
+        'paragraph' => 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s.',
+        'preview' => 'https://www.youtube.com/embed/y6oMutwJQCw',
+        'trailer' => 'https://www.youtube.com/embed/y6oMutwJQCw'
+    ),
+    array(
+        'title' => 'Title 9',
+        'paragraph' => 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s.',
+        'preview' => 'https://www.youtube.com/embed/y6oMutwJQCw',
+        'trailer' => 'https://www.youtube.com/embed/y6oMutwJQCw'
+    ),
+    array(
+        'title' => 'Title 10',
+        'paragraph' => 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s.',
+        'preview' => 'https://www.youtube.com/embed/y6oMutwJQCw',
+        'trailer' => 'https://www.youtube.com/embed/y6oMutwJQCw'
+    ),
+);
