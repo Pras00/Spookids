8cd7d045be69c03d3d33eff9c084e807b1f60d1d
Pras00 <prasetiawahyu22@gmail.com>
2024-05-15 14:57:29 +0700

    [FEAT] Display produk

--- jv-page/produk.php
+++ jv-page/produk.php
@@ -4,54 +4,54 @@ $produk = array(
         'id' => '1',
         'title' => 'The Brave Knight and the Nervous Ghost',
         'paragraph' => 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s.',
-        'img-preview' => 'img/The Brave Knight and the Nervous Ghost.png',
-        'img-trailer' => 'img/The Inhabitant of the Southern Beach.png',
-        'preview' => 'https://www.youtube.com/embed/y6oMutwJQCw?autoplay=1&enablejsapi=1',
-        'trailer' => 'https://www.youtube.com/embed/y6oMutwJQCw?autoplay=1&enablejsapi=1'
+        'img-preview' => 'img/preview1.png',
+        'img-trailer' => 'img/trailer1.png',
+        'preview' => 'https://www.youtube.com/embed/a8oWmzzea0U?autoplay=1&enablejsapi=1',
+        'trailer' => 'https://www.youtube.com/embed/q_BTAWYfvGE?autoplay=1&enablejsapi=1'
     ),
     array(
         'id' => '2',
         'title' => 'The Glowing Goblin Who Lost His Gleam',
         'paragraph' => 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s.',
-        'img-preview' => 'img/The Glowing Goblin Who Lost His Gleam.png',
-        'img-trailer' => 'img/The Inhabitant of the Southern Beach.png',
-        'preview' => 'https://www.youtube.com/embed/y6oMutwJQCw?autoplay=1&enablejsapi=1',
-        'trailer' => 'https://www.youtube.com/embed/y6oMutwJQCw?autoplay=1&enablejsapi=1'
+        'img-preview' => 'img/preview2.png',
+        'img-trailer' => 'img/trailer2.png',
+        'preview' => 'https://www.youtube.com/embed/Lv_7_h9ExXA?autoplay=1&enablejsapi=1',
+        'trailer' => 'https://www.youtube.com/embed/oSNzHXqSsxM?autoplay=1&enablejsapi=1'
     ),
     array(
         'id' => '3',
         'title' => 'The Creaky Doorway That Led to Friendship',
         'paragraph' => 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s.',
-        'img-preview' => 'img/The Creaky Doorway That Led to Friendship.png',
-        'img-trailer' => 'img/The Inhabitant of the Southern Beach.png',
-        'preview' => 'https://www.youtube.com/embed/y6oMutwJQCw?autoplay=1&enablejsapi=1',
-        'trailer' => 'https://www.youtube.com/embed/y6oMutwJQCw?autoplay=1&enablejsapi=1'
+        'img-preview' => 'img/preview3.png',
+        'img-trailer' => 'img/trailer3.png',
+        'preview' => 'https://www.youtube.com/embed/rttWkFYd2_c?autoplay=1&enablejsapi=1',
+        'trailer' => 'https://www.youtube.com/embed/edxKQ-_aDA8?autoplay=1&enablejsapi=1'
     ),
     array(
         'id' => '4',
-        'title' => 'The Monster Under the Bed Who Needed a Hug',
+        'title' => 'Hiccup The Happy Haunter',
         'paragraph' => 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s.',
-        'img-preview' => 'img/Hiccup the Happy Haunter.png',
-        'img-trailer' => 'img/The Inhabitant of the Southern Beach.png',
-        'preview' => 'https://www.youtube.com/embed/y6oMutwJQCw?autoplay=1&enablejsapi=1',
-        'trailer' => 'https://www.youtube.com/embed/y6oMutwJQCw?autoplay=1&enablejsapi=1'
+        'img-preview' => 'img/preview4.png',
+        'img-trailer' => 'img/trailer4.png',
+        'preview' => 'https://www.youtube.com/embed/xlYDUoTxmTc?autoplay=1&enablejsapi=1',
+        'trailer' => 'https://www.youtube.com/embed/R5Azj9J852s?autoplay=1&enablejsapi=1'
     ),
     array(
         'id' => '5',
         'title' => 'The Shy Witch Who Could not Cast a Shadow',
         'paragraph' => 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s.',
-        'img-preview' => 'img/The Shy Witch Who Couldnt Cast a Shadow to Friendship.png',
-        'img-trailer' => 'img/The Inhabitant of the Southern Beach.png',
-        'preview' => 'https://www.youtube.com/embed/y6oMutwJQCw?autoplay=1&enablejsapi=1',
-        'trailer' => 'https://www.youtube.com/embed/y6oMutwJQCw?autoplay=1&enablejsapi=1'
+        'img-preview' => 'img/preview5.png',
+        'img-trailer' => 'img/trailer5.png',
+        'preview' => 'https://www.youtube.com/embed/JylThlx7krw?autoplay=1&enablejsapi=1',
+        'trailer' => 'https://www.youtube.com/embed/3eZPUvgNeys?autoplay=1&enablejsapi=1'
     ),
     array(
         'id' => '6',
         'title' => 'When the Vacuum Cleaner Became Scared',
         'paragraph' => 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s.',
-        'img-preview' => 'img/When the Vacuum Cleaner Became Scared.png',
-        'img-trailer' => 'img/The Inhabitant of the Southern Beach.png',
-        'preview' => 'https://www.youtube.com/embed/y6oMutwJQCw?autoplay=1&enablejsapi=1',
-        'trailer' => 'https://www.youtube.com/embed/y6oMutwJQCw?autoplay=1&enablejsapi=1'
+        'img-preview' => 'img/preview6.png',
+        'img-trailer' => 'img/trailer6.png',
+        'preview' => 'https://www.youtube.com/embed/v_uXAUaHKMQ?autoplay=1&enablejsapi=1',
+        'trailer' => 'https://www.youtube.com/embed/IoUHUAU68wU?autoplay=1&enablejsapi=1'
     ),
 );
